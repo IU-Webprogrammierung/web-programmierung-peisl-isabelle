a37233908f9e70116583c9ab0529b3a8275ee783
isabellepeisl <162800073+isabellepeisl@users.noreply.github.com>
2025-04-03 10:50:08 +0200

    refactor: refactor navigation with Tailwind

--- js/navigation.js
+++ js/navigation.js
@@ -1,23 +1,22 @@
 document.addEventListener('componentsLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navMenu = document.querySelector('.nav-menu');
-    
-    if (menuToggle && navMenu) {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navMenu = document.querySelector('.nav-menu');
+
+  if (menuToggle && navMenu) {
       menuToggle.addEventListener('click', function() {
-        navMenu.classList.toggle('open');
+          navMenu.classList.toggle('hidden');
       });
-    } else {
+  } else {
       console.error("Hamburger button or navigation menu not found.");
-    }
-  
-    const navLinks = document.querySelectorAll('.nav-link');
-    const currentPath = window.location.pathname.replace(/^\/|\/$/g, '');
-    navLinks.forEach(link => {
+  }
+
+  const navLinks = document.querySelectorAll('.nav-link');
+  const currentPath = window.location.pathname.replace(/^\/|\/$/g, '');
+  navLinks.forEach(link => {
       if (link.getAttribute('href') === currentPath) {
-        link.classList.add('active');
+          link.classList.add('active');
       } else {
-        link.classList.remove('active');
+          link.classList.remove('active');
       }
-    });
   });
-  
+});
