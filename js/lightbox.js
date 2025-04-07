4d78b6657605e38271b446993900d6dbea30d70d
isabellepeisl <162800073+isabellepeisl@users.noreply.github.com>
2025-04-07 10:38:57 +0200

    fix: small layout adjustments

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,71 +1,59 @@
-// Alle Galerie-Items
 const galleryItems = document.querySelectorAll('.galerie-item');
 const swipeItems = document.querySelectorAll('.swipe-galerie-item');
 
-// Das Lightbox-Modal und Bild
 const lightbox = document.createElement('div');
 lightbox.classList.add('fixed', 'inset-0', 'bg-black', 'bg-opacity-80', 'flex', 'items-center', 'justify-center', 'z-50', 'hidden');
 document.body.appendChild(lightbox);
 
-// Das Bild im Modal
 const lightboxImg = document.createElement('img');
 lightboxImg.classList.add('max-w-[90%]', 'max-h-[80%]', 'object-contain', 'rounded-lg');
 lightbox.appendChild(lightboxImg);
 
-// Schließen-Button
 const closeBtn = document.createElement('button');
 closeBtn.textContent = '×';
 closeBtn.classList.add('absolute', 'top-5', 'right-5', 'text-3xl', 'text-white', 'bg-black', 'bg-opacity-50', 'p-3', 'rounded-full', 'hover:bg-opacity-70', 'cursor-pointer');
 lightbox.appendChild(closeBtn);
 
-// Öffnet das Modal bei Klick auf ein Bild
 galleryItems.forEach(item => {
     item.addEventListener('click', (event) => {
         event.preventDefault();
         const imgSrc = item.querySelector('img').src;
         lightboxImg.src = imgSrc;
-        lightbox.classList.remove('hidden'); // Zeigt die Lightbox
+        lightbox.classList.remove('hidden');
     });
 });
 
-// Schließt das Modal
 closeBtn.addEventListener('click', () => {
-    lightbox.classList.add('hidden'); // Versteckt das Modal
+    lightbox.classList.add('hidden');
 });
 
-// Schließt das Modal, wenn auf das Overlay geklickt wird
 lightbox.addEventListener('click', (event) => {
     if (event.target === lightbox) {
-        lightbox.classList.add('hidden'); // Versteckt das Modal
+        lightbox.classList.add('hidden');
     }
 });
 
-// Holen der Karussell-Elemente
-const carouselItems = document.querySelectorAll('.carousel-item');
-const carouselContainer = document.querySelector('.carousel-items');
+// Carousel für Mobile Screens
+const carouselItems = document.querySelectorAll('.tattoo-carousel-item');
+const carouselContainer = document.querySelector('.tattoo-carousel-items');
 
-// Index für das aktuelle Bild im Karussell
 let currentIndex = 0;
 
-// Funktion zum Weiterschalten
 function moveToNext() {
-    currentIndex = (currentIndex + 1) % carouselItems.length; // Nächster Index
+    currentIndex = (currentIndex + 1) % carouselItems.length;
     updateCarouselPosition();
 }
 
-// Funktion zum Zurückschalten
 function moveToPrev() {
-    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length; // Vorheriger Index
+    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
     updateCarouselPosition();
 }
 
-// Funktion zum Aktualisieren der Position des Karussells
 function updateCarouselPosition() {
-    const offset = -currentIndex * 100; // 100vw für jedes Bild
+    const offset = -currentIndex * 100;
     carouselContainer.style.transform = `translateX(${offset}vw)`;
 }
 
-// Event Listener für die Pfeile
-document.querySelector('.carousel-next').addEventListener('click', moveToNext);
-document.querySelector('.carousel-prev').addEventListener('click', moveToPrev);
+document.querySelector('.tattoo-carousel-next').addEventListener('click', moveToNext);
+document.querySelector('.tattoo-carousel-prev').addEventListener('click', moveToPrev);
 
--- js/navigation.js
+++ js/navigation.js
@@ -1,22 +1,22 @@
 document.addEventListener('componentsLoaded', function() {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navMenu = document.querySelector('.nav-menu');
-
-  if (menuToggle && navMenu) {
-      menuToggle.addEventListener('click', function() {
-          navMenu.classList.toggle('hidden');
-      });
-  } else {
-      console.error("Hamburger button or navigation menu not found.");
-  }
-
-  const navLinks = document.querySelectorAll('.nav-link');
-  const currentPath = window.location.pathname.replace(/^\/|\/$/g, '');
-  navLinks.forEach(link => {
-      if (link.getAttribute('href') === currentPath) {
-          link.classList.add('active');
-      } else {
-          link.classList.remove('active');
-      }
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+  
+    if (menuToggle && navMenu) {
+        menuToggle.addEventListener('click', function() {
+            navMenu.classList.toggle('hidden');
+        });
+    } else {
+        console.error("Hamburger button or navigation menu not found.");
+    }
+  
+    const navLinks = document.querySelectorAll('.nav-link');
+    const currentPath = window.location.pathname.replace(/^\/|\/$/g, '');
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('font-semibold');
+        } else {
+            link.classList.remove('font-semibold');
+        }
+    });
   });
-});
